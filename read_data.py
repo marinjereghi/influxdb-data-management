5c5885de706890e5c13f4bc4fce9259bfb33eddb
Marin Jereghi <marinjereghi@yahoo.com>
2022-01-25 12:05:21 +0100

    Create query params

--- read_data.py
+++ read_data.py
@@ -1,16 +1,26 @@
 from influxdb_client import InfluxDBClient
-from config import influx_debug, influx_org, influx_token, influx_url
+from config import influx_debug, influx_org, influx_token, influx_url, influx_bucket
 import pandas as pd
 from datetime import datetime
 
 if __name__ == "__main__":
     startTime = datetime.now()
 
+    _start = datetime.fromisoformat("2018-12-31T23:59:59.000")
+    _stop = datetime.fromisoformat("2019-12-31T00:00:00.000")
+
+    params = {
+        "_bucket": influx_bucket,
+        "_start": _start,
+        "_stop": _stop
+        # "_desc": True
+    }
+
     '''
     for each card, get the next swipe time and elapsed time
     '''
     query2 = 'from(bucket: "veronacard1")'\
-        '|> range(start: 2020-12-01T00:00:00Z, stop: 2020-12-01T23:59:59Z)'\
+        '|> range(start: 2013-12-31T23:59:59Z, stop: 2014-05-31T23:59:59Z)'\
         '|> group(columns: ["_value"])'\
         '|> sort(columns: ["_time"])'\
         '|> elapsed()'
@@ -25,27 +35,28 @@ if __name__ == "__main__":
     '''
     for each day, for each point of interest get swiped cards
     '''
-    query = 'from(bucket:"veronacard1")' \
-        '|> range(start: 2020-12-01T00:00:00Z, stop: 2020-12-03T00:00:00Z) ' \
-        '|> keep(columns: ["_time", "_value", "_measurement", "_field", "activation_date", "profile"])'\
+    query = 'from(bucket: _bucket)' \
+        '|> range(start: _start, stop: _stop)' \
         '|> group(columns: ["_measurement"])'\
         '|> aggregateWindow(every: 24h, fn: count, column: "_value")'
+    # '|> sort(columns:["_time"], desc:_desc)'
+    # '|> keep(columns: ["_time", "_value", "_measurement"])'\
 
     client = InfluxDBClient(url=influx_url,
                             token=influx_token, org=influx_org, debug=influx_debug)
-    #result = client.query_api().query(query=query2)
+    # result = client.query_api().query(query=query)
+    # data_frame_index=["_time"]
     result = client.query_api().query_data_frame(
-        query=query2, org=influx_org)
-    # for table in result:
-    #     for data in table.records:
-    #         print(data["elapsed"])
-    #         print(data["table"], data["_measurement"],
-    #               data["_value"], data["_time"])
-    for df in result:
-        if df.empty:
-            print('empty df\t', df)
-        else:
-            df.set_index(df["_time"])
-            df.to_csv('./out.csv', mode='a', sep=";", header=False)
+        query=query, org=influx_org, params=params)
 
-    print(f'Import finished in: {datetime.now() - startTime}')
+    # for df in result:
+    # print(result)
+    # if df.empty:
+    #     print('empty df\t', df)
+    # else:
+    #     df.set_index(df["_time"])
+    print(result.head(7))
+    #result.sort_values('_time', inplace=True)
+    result.to_csv('./_result_2019.csv', mode='a', sep=";", header=False)
+    client.close()
+    print(f'Export finished in: {datetime.now() - startTime}')
